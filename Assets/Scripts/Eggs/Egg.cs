ac5382aa1fe91eb05ef7f19baac17401efa63684
Kelly Chara <fabianachara@gmail.com>
2023-10-21 18:26:38 -0500

    refactor variables

--- Assets/Scripts/Eggs/Egg.cs
+++ Assets/Scripts/Eggs/Egg.cs
@@ -9,13 +9,17 @@ public class Egg : MonoBehaviour
         player = FindObjectOfType<PlayerController>();
     }
    
+
     private void OnTriggerEnter(Collider other)
 {
         if (other.CompareTag("Player") && !player.HasEgg())
         {
             player.pickedEgg = this;
-            SetEggParent(player);
-    }
+            SetEggParent(player); 
+        }else if (other.CompareTag("Player") && player.HasEgg())
+        {
+            Debug.Log("You can only have one egg at a time");
+        }
 }
 
     public void SetEggParent(PlayerController player)

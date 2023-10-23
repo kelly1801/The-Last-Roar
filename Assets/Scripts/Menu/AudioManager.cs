f2a207856d09d291b636123049b0395ffe7a5bc3
andre-cat <andrecat-@outlook.com>
2023-10-22 19:13:49 -0500

    Volume slider added

--- Assets/Scripts/Menu/AudioManager.cs
+++ Assets/Scripts/Menu/AudioManager.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+
+    private static readonly string VOLUME = "volume";
+    
     private AudioManager instance;
+
+    public static float Volume
+    {
+        get { return PlayerPrefs.GetFloat(VOLUME, 0.1f); }
+        set { PlayerPrefs.SetFloat(VOLUME, value);}
+    }
 
     private void Awake()
     {
